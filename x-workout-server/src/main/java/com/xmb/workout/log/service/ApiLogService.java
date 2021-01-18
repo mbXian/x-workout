adfb49111535a7b7d6a530aeb4f8047c56f5d100
Ben <xianminbing@gmail.com>
2021-01-18 17:36:03 +0800

    feat: 植入日志记录框架

---
+++ x-workout-server/src/main/java/com/xmb/workout/log/service/ApiLogService.java
@@ -0,0 +1,14 @@
+package com.xmb.workout.log.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xmb.workout.log.entity.ApiLogEntity;
+
+/**
+ * @author Ben
+ * @date 2021-01-18
+ * @desc
+ */
+public interface ApiLogService  extends IService<ApiLogEntity>  {
+
+
+}
